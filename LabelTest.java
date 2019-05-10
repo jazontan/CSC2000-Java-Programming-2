6a0d38c050ce7b0a612faf51a8d1ef365786ca63
jazontan <42987310+jazontan@users.noreply.github.com>
2019-05-10 12:53:07 +0800

    Add files via upload

---
+++ LabelTest.java
@@ -0,0 +1,13 @@
+// Testing LabelFrame.
+import javax.swing.JFrame;
+
+public class LabelTest
+{
+	public static void main( String[] args )
+	{
+		LabelFrame labelFrame = new LabelFrame(); // create LabelFrame
+		labelFrame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
+		labelFrame.setSize( 280, 280 ); // set frame size
+		labelFrame.setVisible( true ); // display frame
+	} // end main
+} // end class LabelTest
